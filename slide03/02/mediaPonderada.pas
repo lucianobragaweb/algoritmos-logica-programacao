25c572836f339ae3b8c89c1c8103f63906e91de3
lucianobragaweb <lucianobragaweb@gmail.com>
2018-07-20 09:18:15 -0300

    Slide 03 - Ex02

--- slide03/02/mediaPonderada.pas
+++ slide03/02/mediaPonderada.pas
@@ -1,20 +1,21 @@
 program mediaPonderada;
 
 var
-  ID1, ID2, ID3: integer;
-  media: real;
+  N1, N2, N3, media: real;
 
 begin
   writeln('Informe 3 idades para calcular a média ponderada.');
 
   write('Informe a primeira Idade:');
-  readln(ID1);
-  write('Informe a segunda Idade:');
-  readln(ID2);
-  write('Informe a terceira Idade:');
-  readln(ID3);
+  readln(N1);
 
-  media := ((ID1 * 2) + (ID2 * 4) + (ID3 * 6)) / 12;
+  write('Informe a segunda Idade:');
+  readln(N2);
+  
+  write('Informe a terceira Idade:');
+  readln(N3);
+
+  media := ((N1 * 2) + (N2 * 4) + (N3 * 6)) / 12;
 
   writeln(media:2:2);
 end.
